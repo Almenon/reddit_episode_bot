984272017a45d596491534f990bd073a385d95d4
Almenon <No email>
2015-07-16 20:05:36 -0700

    made bot more polite
    bot now scrapes a list of restricted subreddits from /r/bottiquette and
    checks if a mention is in a restricted subreddit

--- bot.py
+++ bot.py
@@ -9,14 +9,12 @@ from requester import get_info
 import logging
 logging.basicConfig(level="DEBUG")
     # set level to INFO to get rid of debug messages
-from re import compile, search
-
+from json import loads
 
 # SETUP
 # string should not contain keyword bot
-user_agent = "Python script for automatic replies with information when my username is tagged with keywords. alpha v.2 by Almenon"
+user_agent = "python script for episode information upon username mention or reply.  alpha v.3 by Almenon"
 r = praw.Reddit(user_agent=user_agent)
-
 
 password = open("password.txt").read()
     # password in text file so it's not availible in public repository
@@ -24,7 +22,11 @@ r.login('the_episode_bot', password, disable_warning=True)
 #todo: use Oauth instead of login
 # must be done by this year because login will be deprecated
 
-already_replied = [] # comments already responded too
+bottiquette = r.get_wiki_page('Bottiquette', 'robots_txt_json')
+# see https://www.reddit.com/r/Bottiquette/wiki/robots_txt_json
+restricted_subreddits = loads(bottiquette.content_md)
+restricted_subreddits = restricted_subreddits["disallowed"] + restricted_subreddits["posts-only"] + restricted_subreddits["permission"]
+
 bot_disclaimer = "------\n" \
                  "^^I'm ^^a ^^bot ^^that ^^gives ^^info ^^on ^^shows. " \
                  "^^| ^^[message](http://www.reddit.com/message/compose?to=the_episode_bot) ^^me ^^if ^^there's ^^an ^^issue. " \
@@ -33,21 +35,27 @@ bot_disclaimer = "------\n" \
 
 while(True):
 
-    messages = r.get_unread()
+    try:
+        messages = r.get_unread()
+    except praw.errors.APIException as e:
+        logging.warning("Reddit might be offline: " + e)
+        sleep(2)
+        continue
 
     for message in messages:
-        # todo: only comment on allowed subreddits
-        # https://www.reddit.com/r/Bottiquette/wiki/robots_txt_json
-        # if message.subreddit is in banned subreddits:
-        #   message person
+        if message.subreddit in restricted_subreddits:
+            r.send_message(message.author,"episodebot could not reply",
+                           "episodebot can't reply to a comment in a restricted subreddit")
+            continue
         answer = get_info(str(message.body))
+        # todo: search reddit for discussion threads
         if answer is not None:
             if answer[0:3] != "400" and answer[0:3] != "404":
                 answer += bot_disclaimer
                 try:
                     message.reply(answer)
                 except Exception as e:
-                    logging.error(str(e))
+                    logging.warning(str(e))
                     # possibly 403 error: sub is private or bot is banned from it
                 sleep(2)
                 message.mark_as_read()
