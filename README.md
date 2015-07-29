b4cfb7d2d07612f02c1f36c1cc14c38dd4ca39e1
Almenon <No email>
2015-07-28 19:43:30 -0700

    Rewrote readme

--- README.md
+++ README.md
@@ -1,16 +1,26 @@
 # reddit episode bot
+
+### features
+
 When it is mentioned with a season and episode number it will return the title, a summary, release date, and rating.
-Still in progress.  Requests should have parameters "show title" episode X season X.
+Additionally, the title links to the imdb page.  Possible features for new version include: Oauth, 
+link to discussion thread, search by title, 24/7 online status via cloud-hosting, support for shows with multiple
+series (ex: doctor who), and a link to netflix if the show is on it.
 
-> Possible requests include, but are not limited to:
+### how to activate
 
-> "show title" season 1 episode 2
+You have to mention /u/the_episode_bot or reply to it to active it.  
+Currently it is only ran at certain times so it will take a while for the bot to reply, if at all.
 
-> "show title" s1e2
+Requests should be in a form similar to this:
 
-> "show title" S01E02
-  
-Of course, you either have to mention /u/the_episode_bot or reply to it to active it.  Currently it is only ran at certain times so it will take a while for the bot to reply, if at all.
+> "show title" season 3 episode 2. 
+
+Regex parsing allows the bot to accept many different request formats, as well as misspellings. The show title 
+can be automatically inferred if the subreddit corresponds to a show in the TV-related subreddits list. 
+Note that the listing is out of date, so please message me if there is a new show to include.
+
 
 Thanks to: [PRAW](https://praw.readthedocs.org/en/v3.1.0/) for conveniently accessing [reddit](https://www.reddit.com/), 
-and [omdbapi](https://www.omdbapi.com) for providing an open-source tv series database.
+[omdbapi](https://www.omdbapi.com) for providing an open-source tv series database, 
+and [TV-related subreddits master-list](http://tv-subreddits.wikidot.com/)
--- bot.py
+++ bot.py
@@ -4,17 +4,18 @@ __author__ = 'Almenon'
 
 
 import praw
-from time import sleep
 import requester
 import commentParser
+from time import sleep
+from json import loads
+from json import load
 import logging
 logging.basicConfig(level="DEBUG")
     # set level to INFO to get rid of debug messages
-from json import loads
 
 # SETUP
-# string should not contain keyword bot
-user_agent = "python script for episode information upon username mention or reply.  alpha v.3 by Almenon"
+user_agent = "python script for episode information upon username mention or reply.  alpha v.4 by Almenon"
+             # string should not contain keyword bot
 r = praw.Reddit(user_agent=user_agent)
 password = open("password.txt").read()
     # password in text file so it's not availible in public repository
@@ -27,18 +28,21 @@ bottiquette = r.get_wiki_page('Bottiquette', 'robots_txt_json')
 restricted_subreddits = loads(bottiquette.content_md)
 restricted_subreddits = restricted_subreddits["disallowed"] + restricted_subreddits["posts-only"] + restricted_subreddits["permission"]
 
+with open("c:/dev/PycharmProjects/reddit_episode_bot/subreddits.txt",'r') as file:
+    subreddit_to_show = load(file) # dictionary [subreddit name] = tv show
+
 bot_disclaimer = "------\n" \
                  "^^I'm ^^a ^^bot ^^that ^^gives ^^info ^^on ^^shows. " \
                  "^^| ^^[message](http://www.reddit.com/message/compose?to=the_episode_bot) ^^me ^^if ^^there's ^^an ^^issue. " \
                  "^^| ^^[about](https://github.com/Almenon/reddit_episode_bot)"
 # formatting for bot_disclaimer thanks to wikipediacitationbot
 
-while(True):
+while True:
 
     try:
         messages = r.get_unread()
     except praw.errors.APIException as e:
-        logging.warning("Reddit might be offline: " + e)
+        logging.warning("Reddit might be offline: " + str(e))
         sleep(2)
         continue
 
@@ -46,19 +50,26 @@ while(True):
         sleep(2)
         try:
             if message.subreddit in restricted_subreddits:
-                r.send_message(message.author,"episodebot could not reply",
+                r.send_message(message.author, "episodebot could not reply",
                                "episodebot can't reply to a comment in a restricted subreddit")
                 continue
             parsedComment = commentParser.parse(message.body)
-            show,season,episode = parsedComment
+            show, season, episode = parsedComment
+            in_tv_subreddit = False
             if show is None:
-                show = "Game of Thrones"
-                # todo: use subreddit title as show title
-                # just print out subreddit title first to see what I get
-                #   use only if show title isn't mentioned in comment
-            # todo: search reddit for discussion threads
-            # restrict search to current subreddit if it's a show subreddit
-            # discussion = r.search(show + ' ' + episode + ' ' + title)
+                try:
+                    show = subreddit_to_show['/r/' + str(message.subreddit)]
+                    in_tv_subreddit = True
+                except KeyError:
+                    r.send_message(message.author, "Please specify a show. If you are in the show's subreddit, "
+                                                   "you shouldn't need to specify it. Message /u/almenon to "
+                                                   "add the subreddit to the TV-related subreddit database")
+                    logging.info("a subreddit was not listed in database")
+                    message.mark_as_read()
+                    continue
+            # todo: search for reddit discussion and put link to first result in comment
+            # (limit feature to certain subreddits because
+            # it might not return correct result in some subreddits)
             answer = requester.get_info(show, season, episode)
             answer += bot_disclaimer
             try:
@@ -68,28 +79,23 @@ while(True):
                 # possibly 403 error: sub is private or bot is banned from it
         except (commentParser.ParseError, requester.CustomError) as error_msg:
             # send error message to me and commenter
-            #r.send_message(message.author, "Episodebot could not reply", "Sorry, " + error_msg)
-            r.send_message("Almenon", "epiosdebot error", str(message) + '\n\n' + error_msg)
+            r.send_message(message.author, "Episodebot could not reply", "Sorry, " + str(error_msg))
+            r.send_message("Almenon", "epiosdebot error", str(message) + '\n\n' + str(error_msg))
 
         message.mark_as_read()
 
     logging.info("sleeping for 30 seconds")
-    sleep(30) # limit is 2 seconds
+    sleep(30)  # limit is 2 seconds. 30 is polite time when bot is in alpha
 
 """
 LINKS AND NOTES:
-for parsing subreddit titles:
-    ex: Gameofthrones
-    determining where spaces are is a problem
-    perhaps I could tell based on capitalization?
-    or look in sidebar where hopefully it is mentioned w/ spaces?
-perhaps instead of getting list of TV subreddits I could use my own list of TV shows and search for related subreddits?
 https://www.reddit.com/r/television
-http://tv-subreddits.wikidot.com/ (TV subreddits masterlist, out of date :(
+http://tv-subreddits.wikidot.com/ (TV subreddits masterlist), out of date :(
 https://www.reddit.com/r/TVSubreddits/top/ for new TV subreddits
+
 https://github.com/reddit/reddit/wiki/OAuth2
 http://praw.readthedocs.org/en/latest/pages/oauth.html
-use pprint for debugging pprint(vars(submission))
+
 reddit bots & utilities: https://github.com/voussoir/reddit
 auto-wiki bot https://github.com/acini/autowikibot-py
 can use https://www.reddit.com/r/FreeKarma/ to get free karma if needed
