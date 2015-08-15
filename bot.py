671d23db750115831f45b523414dead4cf4b4571
Almenon <No email>
2015-08-15 09:11:02 -0700

    Bot now checks post titles.  If a title has a season/episode number it will respond to it with just like how it would to a comment. Currently I'm just doing this on /r/bojackhorseman but I plan to expand to other subreddits.

--- bot.py
+++ bot.py
@@ -4,22 +4,30 @@ __author__ = 'Almenon'
 
 
 import praw
+from time import time
 import requester
 import netflix_requester
 import commentParser
 from time import sleep
 from json import loads
 from json import load
+from requests.exceptions import ConnectionError
+#import sys
+#sys.stderr = open("botlog.txt",'w')
+    # disable this when testing
+    # make sure to periodically check on log
+    # todo: get logging redirect to stderr working
 import logging
-logging.basicConfig(level="DEBUG")
+logging.basicConfig(level="DEBUG",filename="botlog.txt")
     # set level to INFO to get rid of debug messages
 
 # SETUP
-user_agent = "python script for episode information upon username mention or reply.  alpha v.4 by Almenon"
+user_agent = "python script for episode information upon username mention or reply.  alpha v.5 by Almenon"
              # string should not contain keyword bot
 r = praw.Reddit(user_agent=user_agent)
-password = open("password.txt").read()
-    # password in text file so it's not availible in public repository
+with open("password.txt") as file:
+    password = file.read()
+    # password in text file so it's not available in public repository
 r.login('the_episode_bot', password, disable_warning=True)
     #todo: use Oauth instead of login
     # must be done by this year because login will be deprecated
@@ -29,8 +37,13 @@ bottiquette = r.get_wiki_page('Bottiquette', 'robots_txt_json')
 restricted_subreddits = loads(bottiquette.content_md)
 restricted_subreddits = restricted_subreddits["disallowed"] + restricted_subreddits["posts-only"] + restricted_subreddits["permission"]
 
-with open("c:/dev/PycharmProjects/reddit_episode_bot/subreddits.txt",'r') as file:
+subreddit = r.get_subreddit("bojackhorseman")
+
+with open("subreddits.txt") as file:
     subreddit_to_show = load(file) # dictionary [subreddit name] = tv show
+with open("time.txt") as file:
+    last_checked = load(file)
+
 
 bot_disclaimer = "------\n" \
                  "^^I'm ^^a ^^bot ^^that ^^gives ^^info ^^on ^^shows. " \
@@ -42,10 +55,60 @@ while True:
 
     try:
         messages = r.get_unread()
-    except praw.errors.APIException as e:
-        logging.warning("Reddit might be offline: " + str(e))
-        sleep(2)
+        submissions = subreddit.get_new()
+    except ConnectionError as e:
+        logging.warning("Reddit is busy or offline: " + str(e) + "\n sleeping for 10 min")
+        #if last_connection_error < 20 min :
+        #    sleep(3600) # sleep for hour
+        sleep(600) # sleep for 10 min
         continue
+
+    for submission in submissions:
+        try:
+            if submission.created_utc < last_checked:
+                logging.info("no new posts in " + str(submission.subreddit))
+                break
+            logging.info("analyzing " + submission.permalink + "\n created at utc time " + str(submission.created_utc))
+            # parse
+            parsedComment = commentParser.parse(submission.title)
+            show, season, episode = parsedComment
+            # infer show
+            if show is None:
+                try:
+                    show = subreddit_to_show['/r/' + str(submission.subreddit).lower()]
+                    # maybe I can find TV show titles by seperating subreddit titles by caps after all
+                except KeyError:
+                    logging.info("the subreddit is not in subreddits.txt")
+                    continue
+            # get netflix & imdb link, add on disclaimer
+            answer = requester.get_info(show, season, episode)
+            netflix_link = netflix_requester.get_netflix_id(show)
+            if netflix_link is not None:
+                answer += "[**Watch on Netflix**](" + netflix_link + ")\n\n"
+            else:
+                answer += "Not available on Netflix\n\n"
+                logging.warning("bojack should be availible on netflix!")
+            answer += bot_disclaimer
+            # post
+            try:
+                submission.add_comment(answer)
+            except Exception as error_msg:
+                r.send_message("Almenon", "epiosdebot error", str(submission) + '\n\n' + str(error_msg))
+                logging.warning(str(error_msg))
+                # possibly 403 error: sub is private or bot is banned from it
+
+        except requester.CustomError as error_msg:
+            # send error message to me
+            r.send_message("Almenon", "epiosdebot error", str(submission) + '\n\n' + str(error_msg))
+            logging.warning(str(error_msg))
+        except commentParser.ParseError as error_msg:
+            logging.warning(str(error_msg))
+
+    last_checked = time()
+    # another approach would be to save newest submission id
+    # r.get_subreddit("name",place_holder=id) to get content newer than id
+    with open("time.txt",'w') as file:
+        file.write(str(last_checked))
 
     for message in messages:
         sleep(2)
@@ -53,7 +116,10 @@ while True:
             if message.subreddit in restricted_subreddits:
                 r.send_message(message.author, "episodebot could not reply",
                                "episodebot can't reply to a comment in a restricted subreddit")
+                message.mark_as_read()
                 continue
+            #if message.subreddit.description.find(
+            #   needs_spoiler = True
             parsedComment = commentParser.parse(message.body)
             show, season, episode = parsedComment
             in_tv_subreddit = False
@@ -69,28 +135,33 @@ while True:
                     logging.info("a subreddit was not listed in database")
                     message.mark_as_read()
                     continue
-            # todo: search for reddit discussion and put link to first result in comment
-            # (limit feature to certain subreddits because
-            # it might not return correct result in some subreddits)
+
             answer = requester.get_info(show, season, episode)
             netflix_link = netflix_requester.get_netflix_id(show)
             if netflix_link is not None:
                 answer += "[**Watch on Netflix**](" + netflix_link + ")\n\n"
+            else:
+                answer += "Not available on Netflix\n\n"
             answer += bot_disclaimer
             try:
                 message.reply(answer)
             except Exception as e:
+                r.send_message(message.author, "Episodebot could not reply",
+                               "Something went wrong with my bot or your request:\n\n error message: " + str(error_msg))
+                r.send_message("Almenon", "epiosdebot error", str(message) + '\n\n' + str(error_msg))
                 logging.warning(str(e))
                 # possibly 403 error: sub is private or bot is banned from it
         except (commentParser.ParseError, requester.CustomError) as error_msg:
             # send error message to me and commenter
-            r.send_message(message.author, "Episodebot could not reply", "Sorry, " + str(error_msg))
+            r.send_message(message.author, "Episodebot could not reply",
+                            "Something went wrong with my bot or your request:\n\n error message: " + str(error_msg))
             r.send_message("Almenon", "epiosdebot error", str(message) + '\n\n' + str(error_msg))
+            logging.warning(str(error_msg) + "\n comment: " + str(message))
 
         message.mark_as_read()
 
-    logging.info("sleeping for 30 seconds")
-    sleep(30)  # limit is 2 seconds. 30 is polite time when bot is in alpha
+    logging.info("sleeping for 120 seconds")
+    sleep(120)  # limit is 2 seconds. 30 is polite time when bot is in alpha
 
 """
 LINKS AND NOTES:
