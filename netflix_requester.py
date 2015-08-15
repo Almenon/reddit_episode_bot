83a863856368df9f4503681cefb5269abbe4d3f7
Almenon <No email>
2015-08-15 09:46:51 -0700

    Added support for non-unicode characters áéíóúüñ

--- netflix_requester.py
+++ netflix_requester.py
@@ -8,6 +8,12 @@ logging.basicConfig(level="DEBUG")  # set level to INFO to get rid of debug mess
 
 netflix_link = "http://www.netflix.com/title/"
 api_url = 'http://netflixroulette.net/api/api.php?title='
+english_letters = "aeiouun"
+accent_letters = b'\xc3\xa1\xc3\xa9\xc3\xad\xc3\xb3\xc3\xba\xc3\xbc\xc3\xb1'.decode()
+ # above is equivalent to "αινσϊόρ".encode().decode()
+ # i should be able to just type "αινσϊόρ" and be done with it
+ # probably has to do with the project encoding (file - settings - editor - file encodings in pycharm)
+ # but it works so i'm not going to mess with it
 class CustomError(Exception):
     pass
 
@@ -15,7 +21,11 @@ class CustomError(Exception):
 def get_netflix_id(title):
     global netflix_link
     global api_url
+    global accent_letters
+    global english_letters
 
+    for accent, no_accent in zip(accent_letters, english_letters):
+        title = title.replace(accent, no_accent)
     post_request = title.replace(' ', '+')
     url = api_url + post_request
     logging.info(url)
@@ -27,7 +37,7 @@ def get_netflix_id(title):
             logging.info(title + " is not on netflix")
             return None
         else: # yay for syntactic sugar
-            raise CustomError("HTTP error: " + str(e.code))
+            raise CustomError("HTTP error in netflix_requester: " + str(e.code))
 
     json_data = data.read().decode('utf-8')
     logging.debug("json data:" + json_data)
@@ -39,7 +49,7 @@ def get_netflix_id(title):
         logging.info(netflix_link)
         return netflix_link
     else:
-        raise CustomError("netflix roulette returned error in json: \n" + str(parsed_json))
+        raise CustomError("Netflix roulette returned error in json: \n" + str(parsed_json))
 
 # print(get_netflix_id("Attack on Titan")) # uncomment this to test
 # thanks to http://netflixroulette.net/api/
