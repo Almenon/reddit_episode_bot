31ff5c9576d6612f32779cc54f6b13e0d50e18e3
Almenon <username@users.noreply.github.com>
2015-11-14 22:03:56 -0800

    Added a docstring.  Changed function name.  Hardcoded in netflix links for function to try to return in case the website is down.

--- netflix_requester.py
+++ netflix_requester.py
@@ -6,12 +6,16 @@ from json import loads
 import logging
 logging.basicConfig(level="DEBUG")  # set level to INFO to get rid of debug messages
 
+netflix_links = { # hardcoded links for important subreddits in case netflix website fails
+    'bojack horseman': 'http://www.netflix.com/title/80019503',
+    'orange is the new black': 'http://www.netflix.com/title/70242311'
+}
 netflix_url = "http://www.netflix.com/title/"
 api_url = 'http://netflixroulette.net/api/api.php?title='
 english_letters = "aeiouun"
 accent_letters = b'\xc3\xa1\xc3\xa9\xc3\xad\xc3\xb3\xc3\xba\xc3\xbc\xc3\xb1'.decode()
- # above is equivalent to "áéíóúüñ".encode().decode()
- # i should be able to just type "áéíóúüñ" and be done with it
+ # above is equivalent to "Ã¡Ã©Ã­Ã³ÃºÃ¼Ã±".encode().decode()
+ # i should be able to just type "Ã¡Ã©Ã­Ã³ÃºÃ¼Ã±" and be done with it
  # probably has to do with the project encoding (file - settings - editor - file encodings in pycharm)
  # but it works so i'm not going to mess with it
 
@@ -20,11 +24,12 @@ class CustomError(Exception):
     pass
 
 
-def get_netflix_id(title):
-    global netflix_url
-    global api_url
-    global accent_letters
-    global english_letters
+def get_netflix_link(title):
+    """
+    :param title: of a show
+    :return: link to instant watch show on Netflix.  None if show is not listed in netflixroulette
+    :raise:  CustomError if the website is down, JSON has the word error, or link doesn't work
+    """
 
     for accent, no_accent in zip(accent_letters, english_letters):
         title = title.replace(accent, no_accent)
@@ -39,7 +44,13 @@ def get_netflix_id(title):
             logging.info(title + " is not on netflix")
             return None
         else:  # yay for syntactic sugar
-            raise CustomError("HTTP error in netflix_requester: " + str(e.code) + '\n' + url)
+            if e.code == 522: logging.info("connection timed out")
+            if title in netflix_links:
+                logging.warning("HTTP error in netflix_requester:" + str(e.code) + '\n' + url)
+                return netflix_links[title]
+            else:
+                logging.info(title + ' netflix link is not hardcoded in')
+                raise CustomError("HTTP error in netflix_requester: " + str(e.code) + '\n' + url)
 
     json_data = data.read().decode('utf-8')
     logging.debug("json data:" + json_data)
@@ -57,7 +68,7 @@ def get_netflix_id(title):
     else:
         raise CustomError("Netflix roulette returned error in json: \n" + str(parsed_json) + '\n' + url)
 
-# print(get_netflix_id("Attack on Titan")) # uncomment this to test
+print(get_netflix_link("bojack horseman")) # uncomment this to test
     # todo: figure out why this file can't be run on its own (low importance)
     # SyntaxError: Non-UTF-8 code starting with '\xe1' in file C:/dev/PycharmProjects/reddit_episode_bot/netflix_requester.py
     # on line 13, but no encoding declared; see http://python.org/dev/peps/pep-0263/ for details
