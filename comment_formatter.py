6396f24565a42c57d6d82a48bf684bab5836135e
Almenon <username@users.noreply.github.com>
2016-07-26 21:22:34 -0700

    seperated out format_comment into two seperate methods for if I'm replying to a post or to a comment.

--- comment_formatter.py
+++ comment_formatter.py
@@ -23,31 +23,79 @@ lastNetflixEpisode = { # last episode availible on Netflix.  9999 for series alw
 }  # note that this system only supports series with < 100 episodes per season
 
 
-def format_comment(request, subreddit, post):
+class CustomError(Exception):
+    pass
+
+
+def format_post_reply(request, subreddit):
     """
     :param request: comment text or post title
     :param subreddit: PRAW subreddit object or name of subreddit (without the /r/)
-    :param post: True if request is a post, False otherwise
     :return: a paragraph of info and links about the episode, formatted for Reddit
-    :except: if any of the sub-modules called raise an exception it passes right through this module
+    :except: omdb_requester.CustomError, post_parser.ParseError, comment_formatter.CustomError
     """
 
     global subreddit_to_show
     global bot_disclaimer
     released = True
 
-    if post:
-        parsedcomment = post_parser.parse(request)
-        season, episode = parsedcomment
-        show = subreddit_to_show[str(subreddit).lower()]
-        if int(season)+int(episode)/100 > lastNetflixEpisode[show]:
-            released = False
+    parsedcomment = post_parser.parse(request)
+    season, episode = parsedcomment
+    show = subreddit_to_show[str(subreddit).lower()]
+    if int(season)+int(episode)/100 > lastNetflixEpisode[show]:
+        released = False
 
+    episode_info = omdb_requester.get_info(show, season, episode)
+    answer = "#####&#009;  \n######&#009;  \n####&#009;  \n" \
+             "###[{title}](http://www.imdb.com/title/{id})\n\n".format(
+                title=episode_info['Title'], id=episode_info['imdbID'])
+    if episode_info["Plot"] != "N/A":
+        answer += '[Mouseover for a brief summary](#mouseover "' + episode_info["Plot"] + '")\n\n'
     else:
-        parsedcomment = commentparser.parse(request)
-        show, season, episode = parsedcomment
-        if show is None:
-            show = subreddit_to_show[str(subreddit).lower()]
+        raise CustomError
+    if episode_info['Year'].isdigit() and int(episode_info['Year']) > datetime.now().year:
+        released = False
+
+    for key in ('imdbRating', 'Released'):
+        if episode_info[key] != 'N/A':
+            answer += key + ": " + episode_info[key] + "\n\n"
+        else: continue
+
+    if released:
+        try:
+            netflix_link = netflix_requester.get_netflix_link(show)
+            if netflix_link is None:
+                raise netflix_requester.CustomError
+                # bot only looks at posts in subreddits with a netflix episode
+                # so this should never happen
+            else:
+                answer += "[**Watch on Netflix**](" + netflix_link + ")\n\n"
+        except netflix_requester.CustomError as e:
+            logging.warning(e)
+    else:
+        answer += "This episode is not on Netflix yet.\n\n"
+
+    answer += bot_disclaimer
+    return answer
+
+
+def format_comment_reply(request, subreddit):
+    """
+    unlike format post reply, this formatter is more lenient if there's missing data
+    :param request: comment text or post title
+    :param subreddit: PRAW subreddit object or name of subreddit (without the /r/)
+    :return: a paragraph of info and links about the episode, formatted for Reddit
+    :except: omdb_requester.CustomError, commentparser.ParseError
+    """
+
+    global subreddit_to_show
+    global bot_disclaimer
+    released = True
+
+    parsedcomment = commentparser.parse(request)
+    show, season, episode = parsedcomment
+    if show is None:
+        show = subreddit_to_show[str(subreddit).lower()]
 
     episode_info = omdb_requester.get_info(show, season, episode)
     answer = "#####&#009;  \n######&#009;  \n####&#009;  \n" \
@@ -79,8 +127,9 @@ def format_comment(request, subreddit, post):
     answer += bot_disclaimer
     return answer
 
+
 # testing:
 
-# print(format_comment("Princess Luna From S2E3 Luna Eclipsed by daisymane","mylittlepony",True))
+# print(format_post_reply("Hiatus weekly re-watch thread: Season 1 Episode 12 - Call of the Cutie","mylittlepony",True))
 
-# print(format_comment("Summoning /u/the_episode_bot arrow s01e03","episode_bot",post=False))
+# print(format_post_reply("Summoning /u/the_episode_bot arrow s01e03","episode_bot",post=False))
