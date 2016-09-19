d65fa2803a2e85d122e4e633445824b38f597d88
Almenon <caleb.collinsparks@gmail.com>
2016-09-19 12:33:15 -0700

    added simple test for omdb_requester

--- unit_tester.py
+++ unit_tester.py
@@ -1,5 +1,6 @@
 import unittest
 from post_parser import parse,ParseError
+from omdb_requester import get_info,OmdbError
 
 with open('info/badPosts.txt') as f:
     badPosts = f.readlines()
@@ -8,14 +9,26 @@ with open('info/goodPosts.txt') as f:
 
 class TestRegex(unittest.TestCase):
 
-    def test_badPosts(self):
+    def test_bad_posts(self):
         for badPost in badPosts:
             with self.assertRaises(ParseError):
                 parse(badPost)
 
-    def test_goodPosts(self):
+    def test_good_posts(self):
         for goodPost in goodPosts:
             try:
                 parse(goodPost)
             except ParseError:
                 self.fail(goodPost)
+
+class TestOmdb(unittest.TestCase):
+
+    def test_bad_show(self):
+            with self.assertRaises(OmdbError):
+                get_info('seafoijosefijo',1,1)
+
+    def test_good_show(self):
+        try:
+            get_info() # default param is game of thrones, s1e1
+        except OmdbError as e:
+            self.fail(e)
