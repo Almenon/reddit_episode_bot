c67772ba7007b05b6c5ab704012598ad8a66c1dd
Almenon <almenon214@gmail.com>
2017-10-24 21:29:29 -0700

    #11 saving scan time before all errors

--- heroku_timing.py
+++ heroku_timing.py
@@ -32,10 +32,12 @@ while True:
             for key in bot.num_posts: bot.num_posts[key] = 0
             last_day = localtime().tm_mday
 
-    except praw.errors.PRAWException as e:
+    except Exception as e:
         logging.exception(e)
         last_time = time()
         saved_time.update_one({},{'$set':{'time':last_time}})
+        if isinstance(e, KeyboardInterrupt) or isinstance(e, SystemExit):
+            raise
         sleep(180)
     except (ConnectionError, ReadTimeout) as e:
         print("there was an error connecting to reddit.  Check if it's down or if there is no internet connection")
@@ -43,7 +45,7 @@ while True:
         last_time = time()
         saved_time.update_one({},{'$set':{'time':last_time}})
         sleep(1200)  # sleep for 20 min
-
+        
     logging.info("sleeping for 3 minutes")
     print("sleeping")
     sleep(180)  # limit is 2 seconds. 30 is polite time.
