5c50570a2b5bc54e0a1a6f0a9bdb7b85e5480311
Almenon <almenon214@gmail.com>
2018-03-07 20:13:29 -0800

    adding /r/shield to subreddits

--- bot.py
+++ bot.py
@@ -17,7 +17,7 @@ logging.basicConfig(level="INFO",
                     )
 # todo: stop praw debug logs from going to console
 
-subreddits = ["archerfx","bojackhorseman","gravityfalls","mylittlepony","stevenuniverse","blackmirror","episode_bot"]
+subreddits = ["archerfx","bojackhorseman","gravityfalls","mylittlepony","stevenuniverse","blackmirror","episode_bot","shield"]
 
 num_posts = {}
 for s in subreddits:
