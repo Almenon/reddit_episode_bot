045bc97a7d7540b51476dbffa6011188d0a91c95
Almenon <almenon214@gmail.com>
2018-03-13 19:25:02 -0700

    adding user to ignore list

--- bot.py
+++ bot.py
@@ -27,7 +27,7 @@ post_limit = {}
 for s in subreddits:
     post_limit[str(s)] = 4
 
-ignoredUsers = ['AutoModerator','Chaosritter','MeNowDealWithIt']
+ignoredUsers = ['AutoModerator','Chaosritter','MeNowDealWithIt','Turtleduck22']
 
 badComments = []
 # store downvoted comments so i don't repeatedly warn myself of the same bad comment
